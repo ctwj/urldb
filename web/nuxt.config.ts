b0602005832789a59b3e4c196dad42dd32860c79
ctwj <908504609@qq.com>
2025-07-12 07:09:18 +0800

    更新 naive

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -1,10 +1,33 @@
+import AutoImport from 'unplugin-auto-import/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: [
-    '@nuxtjs/tailwindcss',
-    '@pinia/nuxt',
-  ],
+  vite: {
+    plugins: [
+      AutoImport({
+        imports: [
+          {
+            'naive-ui': [
+              'useDialog',
+              'useMessage',
+              'useNotification',
+              'useLoadingBar'
+            ]
+          }
+        ]
+      }),
+      Components({
+        resolvers: [NaiveUiResolver()]
+      })
+    ],
+    optimizeDeps: {
+      include: ['naive-ui', 'vueuc', 'date-fns']
+    }
+  },
+  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
   css: [
     '~/assets/css/main.css',
     'vfonts/Lato.css',
@@ -13,11 +36,11 @@ export default defineNuxtConfig({
   ],
   app: {
     head: {
-      title: '网盘资源管理系统',
+      title: '开源网盘资源管理系统',
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { name: 'description', content: '网盘资源管理系统 - 一个现代化的资源管理系统' }
+        { name: 'description', content: '开源网盘资源管理系统 - 一个现代化的资源管理系统' }
       ],
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -36,4 +59,4 @@ export default defineNuxtConfig({
   nitro: {
     preset: 'node'
   }
-}) 
+})
