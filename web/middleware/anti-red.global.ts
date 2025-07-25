6cbd1f5d170fa4dc954573e2a32e8d6db9bba690
Kerwin <kerwinwang@bestier.net>
2025-07-25 10:48:37 +0800

    update: forbiddenUI

--- web/middleware/anti-red.global.ts
+++ web/middleware/anti-red.global.ts
@@ -3,7 +3,7 @@ export default defineNuxtRouteMiddleware((to, from) => {
   if (process.client) {
     const ua = navigator.userAgent
     // 检测是否为QQ或微信内置浏览器
-    if (ua.includes('QQ/') || ua.includes('MicroMessenger')) {
+    if (['QQ/', 'MicroMessenger', 'WeiBo', 'DingTalk', 'Mail'].some(it => ua.includes(it))) {
       // 如果当前不在禁止访问页面，则跳转
       if (to.path !== '/forbidden') {
         return navigateTo('/forbidden')
